9ce404e448db78db91e0df3d7b90ecc67da5c24e
Diego Sosa <dhsosa98@gmail.com>
2022-04-06 01:57:42 -0300

    created getAllproducts controller

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -9,6 +9,12 @@ async function getProductByID(req, res, next){
     return res.status(404).send(`Product #${id} not found`);
 }
 
+async function getAllProducts(req, res, next){
+    const products = await productService.findAll();
+    return res.status(200).send(products);
+}
+
 module.exports = {
-    getProductByID
+    getProductByID,
+    getAllProducts
 } 
