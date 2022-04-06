eabf0b456780373e2e3f027473eee89609297d39
Diego Sosa <dhsosa98@gmail.com>
2022-04-06 01:58:08 -0300

    created findAll products service

--- src/services/products.js
+++ src/services/products.js
@@ -6,7 +6,16 @@ const products =[{
     price: 122,
     image: '',
     stock: 12,
-}];
+},
+{
+    id: 2,
+    name: 'Product 2',
+    description: 'bla bla 2',
+    price: 433,
+    image: '',
+    stock: 9,
+  },
+];
 
 async function findByPK(id) {
     const product = products.find((item) => item.id == id);
@@ -16,6 +25,10 @@ async function findByPK(id) {
     return product;
 }
 
-const productService = { findByPK };
+async function findAll() {
+    return products;
+}
+
+const productService = { findByPK, findAll };
 
 module.exports = productService;
